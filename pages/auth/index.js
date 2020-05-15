ca915a16b8f3c4a06f7a23062a68ed8b9371c7e5
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-15 10:13:00 +0530

    add pages

---
+++ pages/auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const authIndexPage =() => (
+    <div>
+        <h1>The Auth Index Page</h1>
+    </div>
+);
+
+export default authIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const indexPage =() => (
+    <div>
+        <h1>The main Page</h1>
+    </div>
+);
+
+export default indexPage;
